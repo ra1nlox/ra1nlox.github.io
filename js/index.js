c5e871bdbf8dfa8f0762d9c0d5ef21562aaf4871
ra1nlox <a.borzoi@yandex.com>
2021-05-12 22:39:52 +0300

    Changes to the dialog so that it is compatible with browsers

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-var dialog = document.querySelector('dialog');
-
 document.querySelector('#openDialog').onclick = function() {
-	dialog.show(); // Показываем диалоговое окно
-}
-
-document.querySelector('#closeDialog').onclick = function() {
-	dialog.close(); // Прячем диалоговое окно
+	alert("Artiom - rainforbird@gmail.com\nSerghei - okladno.64454@gmail.com");
 }
